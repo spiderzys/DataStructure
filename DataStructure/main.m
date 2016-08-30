8d60ff31bd5c203e8fcceacb625f1972aab6d96e
spiderzys <spiderzys@hotmail.com>
2016-08-30 17:00:34 -0400

    Initial Commit

---
+++ DataStructure/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DataStructure
+//
+//  Created by YANGSHENG ZOU on 2016-08-30.
+//  Copyright © 2016 YANGSHENG ZOU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
